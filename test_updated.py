c44d0c55189651350c40582da758dcbe57431775
Adyatoni Biswas <126558390+Adyatoni@users.noreply.github.com>
2023-08-06 21:58:56 +0530

    Update test_updated.py

--- test_updated.py
+++ test_updated.py
@@ -1,6 +1,6 @@
 from number_to_bengali.main import to_bn_word
 
-#number = '১৩৪৯'                #তেরশো ঊনপঞ্চাশ 
-number = 123                     #এক শো তেইশ
+#number = '১,৩৪৯'                #তেরশো ঊনপঞ্চাশ 
+number = '1,323'                  #তের শো তেইশ  
 
 print(to_bn_word(number))
